d326ec2992c620a93a24896517036b7ecd332e77
sadaf2605 <sadaf2605@gmail.com>
2013-01-09 18:01:02 +0600

    undone bootstrap due to their bug

---
+++ loginlayout/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loginlayout::Application.config.secret_token = '7cb659febfb9e10db962bdd199085f7c03e47c01f489ed10f8599ec6428a6bdcbdbfabd89953b47b8fadfa6ccb2110d2f537644f3102171396846402e5338b2d'
---
+++ tweeterbootstrap/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweeterbootstrap::Application.config.secret_token = 'df70cac38e99776746872659a0f4d17e417b41acba80b31b5cdc17daf9795aabbda1724eaa342dbfcbe09cd5531495f1d1f7204181c84611004fadca99fe4aea'
